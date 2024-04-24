447c3239478c2148731bb528680ffb0d4489a877
shwetapjsofttech <165642971+shwetapjsofttech@users.noreply.github.com>
2024-04-24 12:20:42 +0530

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,5 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("DevClass+stream2learn");
+        //checkin stream2 learn
     }
 }
