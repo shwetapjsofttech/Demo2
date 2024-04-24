b4c432445825b605074eb9c7516b7294692422c9
shivkumarPjsoftTech <165641797+shivkumarPjsoftTech@users.noreply.github.com>
2024-04-24 12:39:12 +0530

    Update DevClass.java

--- src/main/java/DevClass.java
+++ src/main/java/DevClass.java
@@ -1,5 +1,6 @@
 public class DevClass {
     public static void main(String[] args) {
-        System.out.println("DevClass");
+        System.out.println("DevClass++ Stream2learn");
+        // STREAM 2 LEARN
     }
 }
